5d5b9dc42c76b5168eb1080267af1559d936cd7e
Hiroyuki <kmc2539@kamiyama.ac.jp>
2025-10-15 00:17:49 +0900

    first commit

---
+++ hiro.rb
@@ -0,0 +1,23 @@
+class Hiro < Formula
+  desc "Hiro language CLI and interpreter"
+  homepage "https://github.com/yourname/hiro"
+  url "https://github.com/yourname/hiro/releases/download/v0.1.14/hiro-0.1.14.tar.gz"
+  sha256 "c8294a77b3dcbfb5fbababde0a3c048459c643a65417f182fe608ff772d68995"
+  license "MIT"
+
+  depends_on "node" # Node実行ラッパー方式のため
+
+  def install
+    libexec.install "lib/hiro"
+    bin.install "bin/hiro"
+    # bin配下のhiroがlibexec配下を参照するように書き換え
+    inreplace bin/"hiro", "/usr/local/lib/hiro", libexec/"hiro"
+  end
+
+  test do
+    (testpath/"hello.hiro").write <<~EOS
+      Hiro Osyaberi ==> ("ok")🐧
+    EOS
+    assert_match "ok", shell_output("#{bin}/hiro -npm hello.hiro")
+  end
+end
